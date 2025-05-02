0e004b5f6b0f6b7d83532ad1504cfc75af203ae8
ZakaDev22 <elfakharzakaria22@gmail.com>
2025-05-03 00:00:49 +0100

    feat: enhance login functionality; improve navigation button visibility and add success message on login

--- Login.js
+++ Login.js
@@ -1,5 +1,10 @@
 import { BasURL } from "./BaseURLS.js";
 
+let loginBtn = document.getElementById("loginbtn");
+let registerBtn = document.getElementById("btnRegister");
+let logoutBtn = document.getElementById("btnLogout");
+
+ShowOrHideNavButtons();
 
 document.getElementById("btnLogin").addEventListener("click", async () => {
   let userName = document.getElementById("user-name").value;
@@ -21,32 +26,54 @@ document.getElementById("btnLogin").addEventListener("click", async () => {
       modal = new bootstrap.Modal(modal);
       modal.hide();
     }
-    document.getElementById("loginbtn").classList.add("visually-hidden");
-    document.getElementById("btnRegister").classList.add("visually-hidden");
-    document.getElementById("btnLogout").classList.remove("visually-hidden");
-
-    SuccessLoginMessage();
+    
+    ShowOrHideNavButtons();
+    postMessage(
+      "Aww yeah, you successfully Logged In To The System, Enjow Your Time",
+      "LogOut",
+      "alert-success"
+    );
   } catch (error) {
     alert("Error during login:", error);
   }
 });
 
-function SuccessLoginMessage() {
-  let user = JSON.parse(localStorage.getItem("user"));
+function ShowOrHideNavButtons(){
+
+    let token = localStorage.getItem("token") || "";
+
+    if(token === ""){ 
+    loginBtn.classList.remove("visually-hidden");
+    registerBtn.classList.remove("visually-hidden");
+    logoutBtn.classList.add("visually-hidden");
+    }
+    else{
+        loginBtn.classList.add("visually-hidden");
+        registerBtn.classList.add("visually-hidden");
+        logoutBtn.classList.remove("visually-hidden");
+    }
+}
+function PopUpMessage(message,LogMsg,AlertType) {
+  let user = JSON.parse(localStorage.getItem("user")) || {};
+  if (!user) {
+    console.error("No user data found in localStorage.");
+    return;
+  }
+  
   let userName = user.username || "No User Name";
 
   let div = document.createElement("div");
   div.innerHTML = `
-           <div class="alert alert-success d-flex flex-column justify-content-center align-items-center" role="alert">
+           <div class="alert ${AlertType} d-flex flex-column justify-content-center align-items-center" role="alert">
               <h4 class="alert-heading">Well done ${userName}</h4>
-              <p>Aww yeah, you successfully Logged In To The System, Enjow Your Time</p>
+              <p>${message}</p>
                <hr>
                <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
   <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
   <path d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05"/>
 </svg>
                <hr/>
-              <p class="mb-0">You Still have The Option to Loged Out Anytime 😊</p>
+              <p class="mb-0">You Still have The Option to ${LogMsg} Anytime 😊</p>
            </div>
      `;
 
@@ -70,4 +97,15 @@ function SuccessLoginMessage() {
       document.body.removeChild(div);
     }, 1000);
   }, 3000);
+}
+
+logoutBtn.addEventListener("click", () => {
+  localStorage.removeItem("token");
+  localStorage.removeItem("user");
+  ShowOrHideNavButtons();
+  PopUpMessage("Success, You Have LogOut From The System", "Log In", "alert-danger");
+});
+
+function SuccessLogoutMessage() {
+
 }
