74a2df241ae72d4eb7e0b0b859f54c871046b332
ZakaDev22 <elfakharzakaria22@gmail.com>
2025-05-03 20:18:11 +0100

    feat: update registration form fields with autocomplete attributes for better user experience

--- Register.js
+++ Register.js
@@ -1,46 +1,43 @@
 import { BasURL } from "./BaseURLS.js";
 
-import { registerBtn } from "./BaseFunctionsAndVaraibels.js";
-
-
 // ---------------functions Related To Register Part---------------- //
   
-registerBtn.addEventListener("click", async () => {
+document.getElementById("register-btn").addEventListener("click", async () => {
 
-    let userName = document.getElementById("register-user-name").value;
-    let password = document.getElementById("register-password").value;
-    let Name = document.getElementById("Personal-name").value;
-    let email = document.getElementById("email").value;
-    let image = document.getElementById("image").value;
+  let userName = document.getElementById("register-user-name").value;
+  let password = document.getElementById("register-password").value;
+  let Name = document.getElementById("Personal-name").value;
+  let email = document.getElementById("email").value;
+  let image = document.getElementById("image").value;
 
-    let registerData = {
-        username: userName,
-        password: password,
-        name: Name,
-        email: email,
-        image: image
-    };
-    try {
-        let response = await axios.post(`${BasURL}register`, registerData);
-        localStorage.setItem("token", response.data.token);
-        localStorage.setItem("user", JSON.stringify(response.data.user));
-        
-        let modal = document.getElementById("RegisterModal");
-        let modalInstance = bootstrap.Modal.getInstance(modal);
-        if (modalInstance) {
-        modalInstance.hide();
-        } else {
-        modal = new bootstrap.Modal(modal);
-        modal.hide();
-        }
-        
-        ShowOrHideNavButtons();
-        PopUpMessage(
-        "Aww yeah, you successfully Registered In To The System, Enjow Your Time",
-        "LogOut",
-        "alert-success"
-        );
-    } catch (error) {
-        alert("Error during registration:", error);
+  let registerData = {
+    username: userName,
+    password: password,
+    name: Name,
+    email: email,
+    image: image,
+  };
+  try {
+    let response = await axios.post(`${BasURL}register`, registerData);
+    localStorage.setItem("token", response.data.token);
+    localStorage.setItem("user", JSON.stringify(response.data.user));
+
+    let modal = document.getElementById("RegisterModal");
+    let modalInstance = bootstrap.Modal.getInstance(modal);
+    if (modalInstance) {
+      modalInstance.hide();
+    } else {
+      modal = new bootstrap.Modal(modal);
+      modal.hide();
     }
+
+    ShowOrHideNavButtons();
+    PopUpMessage(
+      "Aww yeah, you successfully Registered In To The System, Enjow Your Time",
+      "LogOut",
+      "alert-success"
+    );
+  } catch (error) {
+    alert("Error during registration:", error);
+  }
 });
