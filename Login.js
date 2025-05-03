e5936a3beb1d464376551fee6e5d2532755be065
ZakaDev22 <elfakharzakaria22@gmail.com>
2025-05-03 17:23:02 +0100

    feat: implement registration functionality in Register.js; add event listener for register button and user feedback

--- Login.js
+++ Login.js
@@ -1,14 +1,10 @@
 import { BasURL } from "./BaseURLS.js";
 import { PopUpMessage } from "./BaseFunctionsAndVaraibels.js";
 
-import { loginBtn } from "./BaseFunctionsAndVaraibels.js";
-import { registerBtn } from "./BaseFunctionsAndVaraibels.js";
 import { logoutBtn } from "./BaseFunctionsAndVaraibels.js";
 
 import {ShowOrHideNavButtons} from "./BaseFunctionsAndVaraibels.js";
 
-
-// functions Related To Loging Part //
 
 
 ShowOrHideNavButtons();
@@ -54,42 +50,3 @@ logoutBtn.addEventListener("click", () => {
   PopUpMessage("Success, You Have LogOut From The System", "Log In", "alert-danger");
 });
 
-// ========== End ============== //
-
-// ---------------functions Related To Register Part---------------- //
-  
-registerBtn.addEventListener("click", async () => {
-    let userName = document.getElementById("user-name").value;
-    let password = document.getElementById("password").value;
-    let email = document.getElementById("email").value;
-    let registerData = {
-        username: userName,
-        password: password,
-        email: email,
-    };
-    try {
-        let response = await axios.post(`${BasURL}register`, registerData);
-        localStorage.setItem("token", response.data.token);
-        localStorage.setItem("user", JSON.stringify(response.data.user));
-        
-        let modal = document.getElementById("RegisterModal");
-        let modalInstance = bootstrap.Modal.getInstance(modal);
-        if (modalInstance) {
-        modalInstance.hide();
-        } else {
-        modal = new bootstrap.Modal(modal);
-        modal.hide();
-        }
-        
-        ShowOrHideNavButtons();
-        PopUpMessage(
-        "Aww yeah, you successfully Registered In To The System, Enjow Your Time",
-        "LogOut",
-        "alert-success"
-        );
-    } catch (error) {
-        alert("Error during registration:", error);
-    }
-});
-
-// ================= End ======= //
